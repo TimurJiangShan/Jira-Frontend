cffca4894fd9cea33b68bfb00fd9372a0cdf185b
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2019-08-23 19:00:20 +1000

    enzyme

--- jest-react/src/App.test.js
+++ jest-react/src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import Enzyme, { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+// mount: 当前组件以及子组件全渲染出来
+// 单元测试用 shallow, 集成测试用 mount
+
+Enzyme.configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const wrapper = mount(<App />);
+  expect(wrapper).toMatchSnapshot();
+  // const container = wrapper.find('[data-test="container"]');
+  // expect(wrapper.find('[data-test="container"]').length).toBe(1);
+  // console.log(wrapper.debug());
+  // expect(wrapper.find('[data-test="container"]').prop('title')).toBe('dell lee');
+  // expect(container).toExist();
+  // expect(container).toHaveProp('title', 'dell lee');
 });
