9b69b84b4af8e4cf4060ebdd02fcdf86251b42c2
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2021-02-24 22:47:19 +1100

    feat: add useAsync custome hook

---
+++ jira/src/utils/useAsync.ts
@@ -0,0 +1,68 @@
+import { useState } from "react";
+
+interface State<D> {
+  error: Error | null;
+  data: D | null;
+  stat: "idle" | "loading" | "error" | "success";
+}
+
+const defaultInitialState: State<null> = {
+  error: null,
+  data: null,
+  stat: "idle",
+};
+
+export const useAsync = <D>(initialState?: State<D>) => {
+  const [state, setState] = useState<State<D>>({
+    ...defaultInitialState,
+    ...initialState,
+  });
+
+  const setData = (data: D) =>
+    setState({
+      data,
+      error: null,
+      stat: "success",
+    });
+
+  const setError = (error: Error) =>
+    setState({
+      error,
+      data: null,
+      stat: "error",
+    });
+
+  // run 用来触发异步请求
+  const run = (promise: Promise<D>) => {
+    if (!promise || !promise.then) {
+      throw new Error("Please pass in Promise type");
+    }
+
+    setState({
+      ...state,
+      stat: "loading",
+    });
+
+    return promise
+      .then((data) => {
+        setData(data);
+        return data;
+      })
+      .catch((error) => {
+        setError(error);
+        return error;
+      });
+  };
+
+  //   stat: "idle" | "loading" | "error" | "success";
+  return {
+    isIdle: state.stat === "idle",
+    isLoading: state.stat === "loading",
+    isError: state.stat === "error",
+    isSuccess: state.stat === "success",
+    run,
+    setData,
+    setError,
+    ...state,
+  };
+};
